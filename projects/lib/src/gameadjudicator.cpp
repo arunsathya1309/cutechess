4df055f99a2b346ea919199761a162c58aa03f6b
arunsathya1309 <arun.sathyamurthy@gmail.com>
2018-12-06 22:17:27 -0800

    Space

--- projects/lib/src/gameadjudicator.cpp
+++ projects/lib/src/gameadjudicator.cpp
@@ -21,72 +21,72 @@
 #include "moveevaluation.h"
 
 GameAdjudicator::GameAdjudicator()
-	: m_drawMoveNum(0),
-	  m_drawMoveCount(0),
-	  m_drawScore(0),
-	  m_drawScoreCount(0),
-	  m_resignMoveCount(0),
-	  m_resignScore(0),
-	  m_maxGameLength(0),
-	  m_tbEnabled(false),
-	  m_tbDrawOnly(false)
+   : m_drawMoveNum(0),
+     m_drawMoveCount(0),
+     m_drawScore(0),
+     m_drawScoreCount(0),
+     m_resignMoveCount(0),
+     m_resignScore(0),
+     m_maxGameLength(0),
+     m_tbEnabled(false),
+     m_tbDrawOnly(false)
 {
-	m_resignScoreCount[0] = 0;
-	m_resignScoreCount[1] = 0;
+   m_resignScoreCount[0] = 0;
+   m_resignScoreCount[1] = 0;
 }
 
 void GameAdjudicator::setDrawThreshold(int moveNumber, int moveCount, int score)
 {
-	Q_ASSERT(moveNumber >= 0);
-	Q_ASSERT(moveCount >= 0);
+   Q_ASSERT(moveNumber >= 0);
+   Q_ASSERT(moveCount >= 0);
 
-	m_drawMoveNum = moveNumber;
-	m_drawMoveCount = moveCount;
-	m_drawScore = score;
-	m_drawScoreCount = 0;
+   m_drawMoveNum = moveNumber;
+   m_drawMoveCount = moveCount;
+   m_drawScore = score;
+   m_drawScoreCount = 0;
 }
 
 void GameAdjudicator::setResignThreshold(int moveCount, int score)
 {
-	Q_ASSERT(moveCount >= 0);
+   Q_ASSERT(moveCount >= 0);
 
-	m_resignMoveCount = moveCount;
-	m_resignScore = score;
-	m_resignScoreCount[0] = 0;
-	m_resignScoreCount[1] = 0;
+   m_resignMoveCount = moveCount;
+   m_resignScore = score;
+   m_resignScoreCount[0] = 0;
+   m_resignScoreCount[1] = 0;
 }
 
 void GameAdjudicator::setMaximumGameLength(int moveCount)
 {
-	Q_ASSERT(moveCount >= 0);
-	m_maxGameLength = moveCount;
+   Q_ASSERT(moveCount >= 0);
+   m_maxGameLength = moveCount;
 }
 
 void GameAdjudicator::setTablebaseAdjudication(bool enable, bool drawOnly)
 {
-	m_tbEnabled = enable;
-	m_tbDrawOnly = drawOnly;
+   m_tbEnabled = enable;
+   m_tbDrawOnly = drawOnly;
 }
 
 void GameAdjudicator::addEval(const Chess::Board* board, const MoveEvaluation& eval)
 {
-	Chess::Side side = board->sideToMove().opposite();
+   Chess::Side side = board->sideToMove().opposite();
 
-	// Tablebase adjudication
-	if (m_tbEnabled)
-	{
-		m_result = board->tablebaseResult();
+   // Tablebase adjudication
+   if (m_tbEnabled)
+   {
+      m_result = board->tablebaseResult();
       if (m_result.isDraw())
       {
          return;
       }
       else
       {
-		   if (m_tbDrawOnly)
+         if (m_tbDrawOnly)
          {
             if (m_result.isNone())
             {
-			      return;
+               return;
             }
             else
             {
@@ -94,72 +94,72 @@ void GameAdjudicator::addEval(const Chess::Board* board, const MoveEvaluation& e
             }
          }
       }
-	}
+   }
 
-	// Moves forced by the user (eg. from opening book or played by user)
-	if (eval.depth() <= 0)
-	{
-		m_drawScoreCount = 0;
-		m_resignScoreCount[side] = 0;
-		return;
-	}
+   // Moves forced by the user (eg. from opening book or played by user)
+   if (eval.depth() <= 0)
+   {
+      m_drawScoreCount = 0;
+      m_resignScoreCount[side] = 0;
+      return;
+   }
 
-	// Draw adjudication
-	if (m_drawMoveNum > 0)
-	{
-		if (qAbs(eval.score()) <= m_drawScore)
-			m_drawScoreCount++;
-		else
-			m_drawScoreCount = 0;
+   // Draw adjudication
+   if (m_drawMoveNum > 0)
+   {
+      if (qAbs(eval.score()) <= m_drawScore)
+         m_drawScoreCount++;
+      else
+         m_drawScoreCount = 0;
 
-		if (board->plyCount() / 2 >= m_drawMoveNum
-		&&  m_drawScoreCount >= m_drawMoveCount * 2)
-		{
-			m_result = Chess::Result(Chess::Result::Adjudication, Chess::Side::NoSide);
-			return;
-		}
-	}
+      if (board->plyCount() / 2 >= m_drawMoveNum
+      &&  m_drawScoreCount >= m_drawMoveCount * 2)
+      {
+         m_result = Chess::Result(Chess::Result::Adjudication, Chess::Side::NoSide);
+         return;
+      }
+   }
 
-	// Resign adjudication
-	if (m_resignMoveCount > 0)
-	{
-		int& count = m_resignScoreCount[side];
-		if (eval.score() <= m_resignScore)
-			count++;
-		else
-			count = 0;
+   // Resign adjudication
+   if (m_resignMoveCount > 0)
+   {
+      int& count = m_resignScoreCount[side];
+      if (eval.score() <= m_resignScore)
+         count++;
+      else
+         count = 0;
 
-		if (count >= m_resignMoveCount)
-		{
-			m_result = Chess::Result(Chess::Result::Adjudication,
-						 side.opposite());
-			return;
-		}
-	}
+      if (count >= m_resignMoveCount)
+      {
+         m_result = Chess::Result(Chess::Result::Adjudication,
+                   side.opposite());
+         return;
+      }
+   }
 
-	// Trust-feature
-	if (eval.isTrusted()
-	&&  eval.score() >= eval.MATE_SCORE - 200)
-	{
-		m_result = Chess::Result(Chess::Result::Adjudication, side, "trusted mate score");
-		return;
-	}
+   // Trust-feature
+   if (eval.isTrusted()
+   &&  eval.score() >= eval.MATE_SCORE - 200)
+   {
+      m_result = Chess::Result(Chess::Result::Adjudication, side, "trusted mate score");
+      return;
+   }
 
-	// Limit game length
-	if (m_maxGameLength > 0
-	&&  board->plyCount() >= 2 * m_maxGameLength)
-	{
-		m_result = Chess::Result(Chess::Result::Adjudication, Chess::Side::NoSide);
-		return;
-	}
+   // Limit game length
+   if (m_maxGameLength > 0
+   &&  board->plyCount() >= 2 * m_maxGameLength)
+   {
+      m_result = Chess::Result(Chess::Result::Adjudication, Chess::Side::NoSide);
+      return;
+   }
 }
 
 void GameAdjudicator::resetDrawMoveCount()
 {
-	m_drawScoreCount = 0;
+   m_drawScoreCount = 0;
 }
 
 Chess::Result GameAdjudicator::result() const
 {
-	return m_result;
+   return m_result;
 }
